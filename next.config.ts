b9d41e924618d9d9b12d9fae115d93e2aa8aaaa9
tureo <dawushi@hotmail.com>
2025-08-17 17:58:43 +0800

    search page loading actions create edit delete error not-found validate auth

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 部分预渲染 ppr
+  // experimental: {
+  //   ppr: 'incremental'
+  // }
 };
 
 export default nextConfig;
